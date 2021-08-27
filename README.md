44258146e8a57a10ff80d97defee96fc33131101
Gioxx <giovanni@solone.it>
2021-08-27 16:20:34 +0200

    Version 1.1

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Block Mail Track
-This is a simple Firefox extension made with URL redirection.
+# Block Mailtrack(.io)
+This is a simple Firefox extension made with URL redirection and some CSS modifications.
 
 # Known Limitations
 
 1) Work only in Gmail
 2) Block only mail tracking from mailtrack.io
-3) Uses rsz.io as image proxy
+
+Original project: https://github.com/ksoftlabs/block-mail-track
--- background.js
+++ background.js
@@ -1,20 +1,22 @@
-var pattern = ["*://*.googleusercontent.com/proxy/*"];
+// Thanks to:
+//  https://stackoverflow.com/a/11554116/2220346
+
+var pattern = ["*://*.googleusercontent.com/proxy/*.png?u=*"];
 var redirectUrl="";
 
 function redirect(requestDetails) {
   if(requestDetails.url.indexOf("mailtrack")==-1){
-    redirectUrl=redirectUrl.replace("https://","https://rsz.io/");
+    redirectUrl=redirectUrl.replace("https://","https://xfiles.noads.it/");
     console.log(requestDetails);
   } else {
-    redirectUrl="https://ksoftlabs.com/wp-content/uploads/2018/10/MailTrackBlocked.jpg";
-    console.log("Redirecting "+ requestDetails.url +" to "+redirectUrl);
+    redirectUrl="https://xfiles.noads.it/img/blockmailtrack/pixel.png";
+    console.log("Redirecting "+ requestDetails.url +" to "+ redirectUrl);
   }
-  
 
   return {
     redirectUrl : redirectUrl
   };
-  
+
 }
 
 browser.webRequest.onBeforeRequest.addListener(
